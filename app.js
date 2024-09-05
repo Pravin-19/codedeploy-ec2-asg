dc5a3b35729ef6d2c8fa112933c77552e76ba980
Pravin V <pravinseo01@gmail.com>
2024-09-05 09:27:16 +0530

    Updated Profile

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ const port = process.env.PORT || 3000;
 
 // Root route
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from EC2 Instance!</h1><p>Welcome to my simple web application.</p>');
+  res.send(`
+    <h1>About</h1>
+    <p><strong>Name:</strong> Pravin V</p>
+    <p><strong>LinkedIn:</strong> <a href="https://linkedin.com/in/pravin-velusamy" target="_blank">linkedin.com/in/pravin-velusamy</a></p>
+    <p><strong>GitHub:</strong> <a href="https://github.com/Pravin-19" target="_blank">github.com/Pravin-19</a></p>
+    <p><strong>Twitter:</strong> <a href="https://x.com/pravinv_" target="_blank">x.com/pravinv_</a></p>
+    <p><strong>Resume:</strong> <a href="https://drive.google.com/file/d/1Rw8H4P9tNWKiOGN9sOkoQ5ffkbOzwLj9/view?usp=sharing" target="_blank">Resume-GoogleDrive</a></p>
+    <p><strong>Code:</strong> <a href="https://github.com/Pravin-19/codedeploy-ec2-asg.git" target="_blank">Github Code</a></p>
+  `);
 });
 
 // /data route
-app.get('/data', (req, res) => {
+app.get('/user', (req, res) => {
   res.json({
-    name: 'Pravin V',
-    role: 'Cloud Engineer',
-    message: 'Passionate about Cloud and DevOps!',
+    Name: 'Pravin V',
+    Role: 'Cloud Engineer',
+    Message: 'Passionate about Cloud and DevOps ',
   });
 });
 
